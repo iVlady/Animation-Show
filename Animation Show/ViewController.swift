bb233c72d054f4f17137df51fe14a4e597e7bd63
iVlady <vinogrotskiy97@gmail.com>
2021-06-23 15:47:53 +0300

    Started the project

--- Animation Show/ViewController.swift
+++ Animation Show/ViewController.swift
@@ -21,11 +21,45 @@ class ViewController: UIViewController {
     }
 
     @IBAction func animateSpringView(_ sender: Any) {
+        
+        flipX()
+    }
+    
+}
+
+
+extension ViewController {
+    func squeeze() {
         springView.animation = "squeeze"
         springView.duration = 3.0
-        springView.force = 2.5
+        springView.force = 1
         springView.animate()
     }
+    
+    func shake() {
+        springView.animation = "shake"
+        springView.duration = 1.0
+        springView.force = 1.4
+        springView.rotate = 0.2
+        springView.animate()
+    }
+    
+    func wobble() {
+        springView.animation = "wobble"
+        springView.duration = 1.5
+        springView.force = 2
+        springView.curve = "easeInOut"
+        springView.animate()
+    }
+    
+    func flipX() {
+        springView.animation = "flipX"
+        springView.duration = 0.5
+        springView.force = 2
+        springView.curve = "easeIn"
+        springView.animate()
+    }
+    
     
 }
 
