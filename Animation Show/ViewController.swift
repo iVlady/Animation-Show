f222a5e8e311cfd3841a8e3f16a510854b4b760e
iVlady <vinogrotskiy97@gmail.com>
2021-06-23 16:21:08 +0300

    created functions and enum

--- Animation Show/ViewController.swift
+++ Animation Show/ViewController.swift
@@ -16,13 +16,85 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-   
-        
     }
 
     @IBAction func animateSpringView(_ sender: Any) {
         
-        flipX()
+        flash()
+    }
+    
+    enum AnimationTypes: String {
+        case squeeze = """
+            springView.animation = "squeeze"
+            springView.duration = 3.0
+            springView.force = 1.0
+            springView.animate()
+        """
+        case shake = """
+            springView.animation = "shake"
+            springView.duration = 1.0
+            springView.force = 1.4
+            springView.rotate = 0.2
+            springView.animate()
+        """
+        case wobble = """
+            springView.animation = "wobble"
+            springView.duration = 1.5
+            springView.force = 2.0
+            springView.curve = "easeInOut"
+            springView.animate()
+        """
+        case flipX = """
+            springView.animation = "flipX"
+            springView.duration = 0.4
+            springView.force = 2.0
+            springView.curve = "easeIn"
+            springView.animate()
+        """
+        case squeezeRight = """
+            springView.animation = "squeezeRight"
+            springView.duration = 0.8
+            springView.force = 3.0
+            springView.curve = "easeInSine"
+            springView.animate()
+        """
+        case slideDown = """
+            springView.animation = "slideDown"
+            springView.duration = 1.4
+            springView.force = 1.0
+            springView.animate()
+        """
+        case fadeInLeft = """
+            springView.animation = "fadeInLeft"
+            springView.duration = 2.0
+            springView.force = 3.4
+            springView.curve = "easeIn"
+            springView.velocity = 1.0
+            springView.damping = 0.4
+            springView.animate()
+        """
+        case fadeInUp = """
+            springView.animation = "fadeInUp"
+            springView.duration = 1.4
+            springView.force = 3.4
+            springView.curve = "easeIn"
+            springView.velocity = 1.0
+            springView.animate()
+        """
+        case zoomIn = """
+            springView.animation = "zoomIn"
+            springView.duration = 0.6
+            springView.force = 3.0
+            springView.curve = "easeOutQuad"
+            springView.animate()
+        """
+        case flash = """
+            springView.animation = "flash"
+            springView.duration = 0.8
+            springView.force = 1.0
+            springView.curve = "easeOutExpo"
+            springView.animate()
+        """
     }
     
 }
@@ -32,7 +104,7 @@ extension ViewController {
     func squeeze() {
         springView.animation = "squeeze"
         springView.duration = 3.0
-        springView.force = 1
+        springView.force = 1.0
         springView.animate()
     }
     
@@ -47,19 +119,67 @@ extension ViewController {
     func wobble() {
         springView.animation = "wobble"
         springView.duration = 1.5
-        springView.force = 2
+        springView.force = 2.0
         springView.curve = "easeInOut"
         springView.animate()
     }
     
     func flipX() {
         springView.animation = "flipX"
-        springView.duration = 0.5
-        springView.force = 2
+        springView.duration = 0.4
+        springView.force = 2.0
         springView.curve = "easeIn"
         springView.animate()
     }
     
+    func squeezeRight() {
+        springView.animation = "squeezeRight"
+        springView.duration = 0.8
+        springView.force = 3.0
+        springView.curve = "easeInSine"
+        springView.animate()
+    }
     
+    func slideDown() {
+        springView.animation = "slideDown"
+        springView.duration = 1.4
+        springView.force = 1.0
+        springView.animate()
+    }
+    
+    func fadeInLeft() {
+        springView.animation = "fadeInLeft"
+        springView.duration = 2.0
+        springView.force = 3.4
+        springView.curve = "easeIn"
+        springView.velocity = 1.0
+        springView.damping = 0.4
+        springView.animate()
+    }
+    
+    func fadeInUp() {
+        springView.animation = "fadeInUp"
+        springView.duration = 1.4
+        springView.force = 3.4
+        springView.curve = "easeIn"
+        springView.velocity = 1.0
+        springView.animate()
+        }
+    
+    func zoomIn() {
+        springView.animation = "zoomIn"
+        springView.duration = 0.6
+        springView.force = 3.0
+        springView.curve = "easeOutQuad"
+        springView.animate()
+    }
+    
+    func flash() {
+        springView.animation = "flash"
+        springView.duration = 0.8
+        springView.force = 1.0
+        springView.curve = "easeOutExpo"
+        springView.animate()
+    }
 }
 
